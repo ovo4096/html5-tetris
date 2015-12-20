c3c207f6d6c8af5dab8fa5df447af5d7e8359ab4
hao <ovo4096@gmail.com>
2015-12-21 02:44:05 +0800

    添加一些绘制测试

--- js/cosco.js
+++ js/cosco.js
@@ -1,49 +1,52 @@
-(function () {
-    Cosco = {
-        //settings: {
-        //    width: 0,
-        //    height: 0,
-        //    fullScreenMode: false,
-        //    canvas: document
-        //},
-        new: function (settings) {
-            var context = canvas.getContext("2d");
+Cosco = {
+    new: function (settings) {
+        var self = {}; // ~= this
 
-            var self = {};
-            var updateTimer;
+        var context;
+        var updateTimer;
 
-            self.init = function () {
-                console.log("[DEBUG]:Cosco init");
-                canvas.width = settings.width;
-                canvas.height = settings.height;
-            };
+        self.init = function () {
+            console.log("[DEBUG]:Cosco init");
+            context = settings.canvas.getContext("2d");
+            settings.canvas.width = settings.width;
+            settings.canvas.height = settings.height;
+        };
 
-            self.start = function () {
-                console.log("[DEBUG]:Cosco start");
-                self.init();
-                updateTimer = setInterval(self.update, 3000);
-            };
+        self.start = function () {
+            console.log("[DEBUG]:Cosco start");
+            self.init();
+            updateTimer = setInterval(self.update, 1000);
+        };
 
-            self.exit = function () {
-                console.log("[DEBUG]:Cosco exit");
-                clearInterval(updateTimer);
-            };
+        self.exit = function () {
+            console.log("[DEBUG]:Cosco exit");
+            clearInterval(updateTimer);
+        };
 
-            self.update = function () {
-                console.log("[DEBUG]:Cosco update");
-                self.updateKeyboard();
-                self.updateDraw();
-            };
+        self.update = function () {
+            console.log("[DEBUG]:Cosco update");
+            self.updateKeyboard();
+            self.updateDraw();
+        };
 
-            self.updateKeyboard = function () {
-                console.log("[DEBUG]:Cosco updateKeyboard");
-            };
+        self.updateKeyboard = function () {
+            console.log("[DEBUG]:Cosco updateKeyboard");
+        };
 
-            self.updateDraw = function () {
-                console.log("[DEBUG]:Cosco updateDraw");
-            };
+        self.updateDraw = function () {
+            console.log("[DEBUG]:Cosco updateDraw");
+            context.clearRect(0,0, settings.width, settings.height);
+            context.fillStyle = "#66CCFF";
+            context.fillRect(0, 0, settings.width, settings.height);
 
-            return self;
-        }
-    };
-}());
+            context.font = "30px Arial";
+            context.fillStyle = "#333";
+            context.fillText("Hello World", 0, 30);
+
+            context.fillStyle = "#666";
+            context.fillText("Hello World", 0, 60);
+        };
+
+        return self;
+    }
+};
