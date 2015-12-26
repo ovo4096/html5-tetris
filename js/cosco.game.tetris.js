fa9aec1409f23fe9bdcde97cb278c8c62f3015d6
hao <ovo4096@gmail.com>
2015-12-27 01:59:23 +0800

    优化命名空间

--- js/cosco.game.tetris.js
+++ js/cosco.game.tetris.js
@@ -1,32 +1,35 @@
 (function () {
     using(Cosco.Game, this);
 
-    TetrisGame = {
-        new: function (settings) {
-            var self = Game.new(settings);
+    Cosco.Demo = {};
+    namespace(Cosco.Demo, {
+        TetrisGame: {
+            new: function (settings) {
+                var self = Game.new(settings);
 
-            self.init = function () {
-                console.log("[DEBUG]:Tetris Game init");
-            };
+                self.init = function () {
+                    console.log("[DEBUG]:Tetris Game init");
+                };
 
-            self.destroy = function () {
-                console.log("[DEBUG]:Tetris Game destroy");
+                self.destroy = function () {
+                    console.log("[DEBUG]:Tetris Game destroy");
 
-                self.clear("#fff");
-            };
+                    self.clear("#fff");
+                };
 
-            self.update = function (timestamp) {
-                console.log("[DEBUG]:Tetris Game update " + timestamp);
-            };
+                self.update = function (timestamp) {
+                    console.log("[DEBUG]:Tetris Game update " + timestamp);
+                };
 
-            self.draw = function (timestamp, ctx) {
-                console.log("[DEBUG]:Tetris Game draw " + timestamp);
-                ctx.fillStyle = "#fff";
-                ctx.font = "16px Arial";
-                ctx.fillText("Hello World", 0, 16);
-            };
+                self.draw = function (timestamp, ctx) {
+                    console.log("[DEBUG]:Tetris Game draw " + timestamp);
+                    ctx.fillStyle = "#fff";
+                    ctx.font = "16px Arial";
+                    ctx.fillText("Hello World", 0, 16);
+                };
 
-            return self;
+                return self;
+            }
         }
-    };
+    });
 }());
--- js/cosco.js
+++ js/cosco.js
@@ -1,77 +1,88 @@
-Cosco = {};
-
-Cosco.Game = {
-    Game: {
-        new: function (settings) {
-            var self = {}; // ~= this
-
-            var ctx;
-            var updateAnimationFrame;
-
-            self.start = function () {
-                console.log("[DEBUG]:Cosco start");
-                init();
-                updateAnimationFrame = window.requestAnimationFrame(update);
-            };
-
-            self.exit = function () {
-                console.log("[DEBUG]:Cosco exit");
-                window.cancelAnimationFrame(updateAnimationFrame);
-                destroy();
-            };
-
-            self.clear = function (color) {
-                ctx.clearRect(0, 0, settings.width, settings.height);
-                ctx.fillStyle = color;
-                ctx.fillRect(0, 0, settings.width, settings.height);
-            };
-
-            function init() {
-                console.log("[DEBUG]:Cosco init");
-                ctx = settings.canvas.getContext("2d");
-                settings.canvas.width = settings.width;
-                settings.canvas.height = settings.height;
-
-                if (typeof self.init !== "undefined") {
-                    self.init();
-                }
-            }
-
-            function destroy() {
-                console.log("[DEBUG]:Cosco destroy");
-
-                if (typeof self.destroy !== "undefined") {
-                    self.destroy();
-                }
-            }
-
-            function update(timestamp) {
-                console.log("[DEBUG]:Cosco update " + timestamp);
-
-                if (typeof self.update !== "undefined") {
-                    self.update(timestamp);
-                }
-
-                draw(timestamp, ctx);
-                updateAnimationFrame = window.requestAnimationFrame(update);
-            }
-
-            function draw(timestamp, ctx) {
-                console.log("[DEBUG]:Cosco updateDraw");
-                self.clear("#66CCFF");
-
-                if (typeof self.draw !== "undefined") {
-                    self.draw(timestamp, ctx);
-                }
-            }
-
-            return self;
-        }
-    }
-};
-
 function using(namespace, self) {
     for (var _class in namespace) {
-        self[_class] = namespace[_class];
+        if (typeof self[_class] === "undefined") {
+            self[_class] = namespace[_class];
+        }
     }
 }
+
+function namespace(namespace, self) {
+    namespace = typeof namespace === "undefined" ? {} : namespace;
+    using(self, namespace);
+}
+
+Cosco = typeof Cosco === "undefined" ? {} : Cosco;
+namespace(Cosco);
+
+(function () {
+    Cosco.Game = typeof Cosco.Game === "undefined" ? {} : Cosco.Game;
+    namespace(Cosco.Game, {
+        Game: {
+            new: function (settings) {
+                var self = {}; // ~= this
+
+                var ctx;
+                var updateAnimationFrame;
+
+                self.start = function () {
+                    console.log("[DEBUG]:Cosco start");
+                    init();
+                    updateAnimationFrame = window.requestAnimationFrame(update);
+                };
+
+                self.exit = function () {
+                    console.log("[DEBUG]:Cosco exit");
+                    window.cancelAnimationFrame(updateAnimationFrame);
+                    destroy();
+                };
+
+                self.clear = function (color) {
+                    ctx.clearRect(0, 0, settings.width, settings.height);
+                    ctx.fillStyle = color;
+                    ctx.fillRect(0, 0, settings.width, settings.height);
+                };
+
+                function init() {
+                    console.log("[DEBUG]:Cosco init");
+                    ctx = settings.canvas.getContext("2d");
+                    settings.canvas.width = settings.width;
+                    settings.canvas.height = settings.height;
+
+                    if (typeof self.init !== "undefined") {
+                        self.init();
+                    }
+                }
+
+                function destroy() {
+                    console.log("[DEBUG]:Cosco destroy");
+
+                    if (typeof self.destroy !== "undefined") {
+                        self.destroy();
+                    }
+                }
+
+                function update(timestamp) {
+                    console.log("[DEBUG]:Cosco update " + timestamp);
+
+                    if (typeof self.update !== "undefined") {
+                        self.update(timestamp);
+                    }
+
+                    draw(timestamp, ctx);
+                    updateAnimationFrame = window.requestAnimationFrame(update);
+                }
+
+                function draw(timestamp, ctx) {
+                    console.log("[DEBUG]:Cosco updateDraw");
+                    self.clear("#66CCFF");
+
+                    if (typeof self.draw !== "undefined") {
+                        self.draw(timestamp, ctx);
+                    }
+                }
+
+                return self;
+            }
+        }
+    });
+}());
